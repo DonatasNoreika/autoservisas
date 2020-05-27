d44dd94892b1bce3a5d47b22f76f31288a6009d8
Donatas Noreika <donoras@gmail.com>
2020-05-27 12:44:18 +0300

    7 (8) paskaita padaryta: DONE - papildytas tinymce funkcionalumas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -139,3 +139,31 @@ EMAIL_HOST_USER = EMAIL_HOST_USER
 # el. pašto adresas iš kurio siųsite
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 # slaptažodis
+
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
