d4076d1e90038592a7ad85316b0e4b64250a7542
Donatas Noreika <donoras@gmail.com>
2020-06-02 10:45:59 +0300

    9 (10) paskaita padaryta: Padaryti, kad profilio puslapyje būtų galima redaguoti vartotojo informaciją, pakeisti nuotrauką.

---
+++ mysite/autoservice/forms.py
@@ -0,0 +1,16 @@
+from .models import Profilis
+from django import forms
+from django.contrib.auth.models import User
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfilisUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profilis
+        fields = ['nuotrauka']
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.forms import User
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfilisUpdateForm
 
 
 def index(request):
@@ -88,7 +89,23 @@ def register(request):
 
 @login_required
 def profilis(request):
-    return render(request, 'profilis.html')
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfilisUpdateForm(request.POST, request.FILES, instance=request.user.profilis)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Profilis atnaujintas")
+            return redirect('profilis')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfilisUpdateForm(instance=request.user.profilis)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'profilis.html', context)
 
 class UzsakymaiListView(generic.ListView):
     model = Uzsakymas
